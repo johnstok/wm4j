e41ce80bdfec70ccb79751458e2ac810255dd653
Keith Webster Johnston <k.w.johnston@gmail.com>
2012-02-05 19:09:39 +0000

    Corrected license on most files.

--- src/main/java/wm/negotiation/Negotiator.java
+++ src/main/java/wm/negotiation/Negotiator.java
@@ -2,20 +2,7 @@
  * Copyright © 2011 Keith Webster Johnston.
  * All rights reserved.
  *
- * This file is part of wm4j.
- *
- * wm4j is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * wm4j is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with wm4j. If not, see <http://www.gnu.org/licenses/>.
+ * Revision      $Rev$
  *---------------------------------------------------------------------------*/
 package wm.negotiation;
 
